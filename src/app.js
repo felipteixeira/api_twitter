15fcdc293ebf3e3147288fc128a151c7919dd50a
Felipe Teixeira <felipe.teixeira@willbank.com.br>
2021-03-28 11:47:19 -0300

    process.env include

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+require('dotenv/config');
 
-const token = 'Bearer AAAAAAAAAAAAAAAAAAAAADNnNgEAAAAAsZyhS8O9r2Ea1zr8TCWIcDxIzXw%3DasXSU3q8zV8T4pz65sfFdv6WoOdD3ZlpCiax4LxyYCRe1YzgRN'
+const token = process.env.TOKEN
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.get("/tweet", (request, response) => {
   return axios.get(`https://api.twitter.com/2/tweets/search/recent?query=${encodeURIComponent(`#${request.query.hashtag}`)}&max_results=20&expansions=author_id&user.fields=public_metrics`,
     { headers: { Authorization: token } }
   ).then(result => {
-     response.status(200).send(result.data)
+    response.status(200).send(result.data)
   }).catch((err) => {
     console.error(err)
   });
